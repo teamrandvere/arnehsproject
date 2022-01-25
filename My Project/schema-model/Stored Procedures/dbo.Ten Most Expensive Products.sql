8b76d8a897ab1ca0d01c496f034f4261e61d37ae
WIN2016\redgate <redgate@redgate.com>
2022-01-25 00:48:09 +0000

    schema initial commit

---
+++ My Project/schema-model/Stored Procedures/dbo.Ten Most Expensive Products.sql
@@ -0,0 +1,10 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+create procedure [dbo].[Ten Most Expensive Products] AS
+SET ROWCOUNT 10
+SELECT Products.ProductName AS TenMostExpensiveProducts, Products.UnitPrice
+FROM Products
+ORDER BY Products.UnitPrice DESC
+GO
